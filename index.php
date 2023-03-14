4fdc3884dad69002d991a4ad4fd66eb317c10e67
kumi0607 <d935.yksmdatti@gmail.com>
2023-03-14 22:54:00 +0900

    cssとimages

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品管理アプリ</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- google fontsの読込 -->
+    <link rel="preconnecct" href="https://fonts.googleapis.com">
+    <link rel="preconnecct" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.php">商品管理アプリ</a>
+        </nav>
+    </header>
+
+    <main>
+        <article class="home">
+            <h1>商品管理アプリ</h1>
+            <p>『PHPとデータベースを連携しよう』成果物</p>
+            <a href="read.php" class="btn">商品一覧</a>
+        </article>
+    </main>
+
+    <footer>
+        <p class="copyright">&copy; 商品アプリ ALL rights reserved.</p>
+    </footer>
+</body>
+</html>
